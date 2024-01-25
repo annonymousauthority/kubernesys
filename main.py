a5d09de7718aded9bcf3a728089c87b3e610ab94
Augustine Francis <enyiaugustinefrancis@gmail.com>
2024-01-25 11:44:21 +0100

    update on entry point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, FastAPI
-
+import os
 from routers import upload
 
 app = FastAPI()
@@ -9,3 +9,12 @@ app.include_router(upload.router)
 @app.get("/")
 async def root():
     return {"Information": "This is the root of the application"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    host = os.environ.get("HOST", "0.0.0.0")
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(app, host=host, port=port, reload=True)
